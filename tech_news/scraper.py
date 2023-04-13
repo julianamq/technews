ffe92cdc80c251e337312afbcef60dd7e65501e1
Juliana <julianamq20@gmail.com>
2023-04-13 16:27:24 -0300

    requisito 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+from bs4 import BeautifulSoup
 
 
 def fetch(url):
@@ -20,13 +21,19 @@ def fetch(url):
 def scrape_updates(html_content):
     selector = Selector(text=html_content)
     links = selector.css(".entry-title a::attr(href)").getall()
-    #print("ju", links)
+    # print("ju", links)
     return links
 
 
 # Requisito 3
+# https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/290e715d-73e3-4b2d-a3c7-4fe113474070/section/7e82ac53-a588-412b-95a5-19727d70ed3a/day/a830c6e2-bcb7-4124-a230-453a8cd952bf/lesson/ba10411c-a220-4589-98af-6653fe9bd525
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    soup = BeautifulSoup(html_content, 'html.parser')
+    next_page_link = soup.find('a', class_="next page-numbers")
+    if (next_page_link):
+        return next_page_link["href"]
+    else:
+        return None
 
 
 # Requisito 4
